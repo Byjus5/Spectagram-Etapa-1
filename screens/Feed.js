97e0b01981fd2ad916298921a68d0d4df3a614a9
Byjus5 <89545408+Byjus5@users.noreply.github.com>
2021-11-30 13:04:51 -0600

    Update Feed.js

--- screens/Feed.js
+++ screens/Feed.js
@@ -10,7 +10,7 @@ export default class Feed extends Component {
                     justifyContent: "center",
                     alignItems: "center"
                 }}>
-                <Text>Feeds</Text>
+                <Text>Índice</Text>
             </View>
         )
     }
